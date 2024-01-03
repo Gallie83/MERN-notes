85958ee71842456207caa321f47ef145ee373c72
Gallie83 <keving2913@hotmail.com>
2024-01-03 21:59:47 +0100

    Added basic front end placeholders with styling and dropdown menus

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,7 +1,56 @@
+import React from 'react';
+import './App.css';
+import Dropdown from "react-bootstrap/Dropdown";
+
+
+const CustomToggle = React.forwardRef(({ onClick }, ref) => (
+  <btn
+    ref={ref}
+    onClick={e => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    <span className="threedots" />
+  </btn>
+));
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-danger">Hello World</h1>
+      <h1 className="m-3">My Notes</h1>
+      <div className="notes-div row">
+
+        <div className="notes p-3 border border-2 rounded col-5 m-4">
+
+          <div className="note-body ps-5">Walk the dog</div>
+
+          <Dropdown>
+            <Dropdown.Toggle as={CustomToggle} />
+            <Dropdown.Menu className='dropdown-menu'>
+              <Dropdown.Item>Edit</Dropdown.Item>
+              <Dropdown.Item>Mark as complete</Dropdown.Item>
+              <Dropdown.Item>Delete</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+
+        <div className="notes is-complete p-3 border border-2 rounded col-5 m-4">
+
+          <div className="note-body ps-5">Take out trash</div>
+
+          <Dropdown>
+            <Dropdown.Toggle as={CustomToggle} />
+            <Dropdown.Menu className='dropdown-menu'>
+              <Dropdown.Item>Edit</Dropdown.Item>
+              <Dropdown.Item>Mark as complete</Dropdown.Item>
+              <Dropdown.Item>Delete</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+        </div>
+      </div>
     </div>
   );
 }
--- notes/src/index.js
+++ notes/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Sets background color for whole page
+document.body.style.backgroundColor = "#192734";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
